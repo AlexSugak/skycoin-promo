ead719719b4b6024f2663ef90dea67d91054b544
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-05-23 20:48:57 +0300

    Add decimal and vendor libraries

--- db/models/user.go
+++ db/models/user.go
@@ -3,29 +3,31 @@ package models
 import (
 	"database/sql"
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 // RegisteredUser represents a gathered data about a user
 type RegisteredUser struct {
-	ID                    int64          `db:"Id"`
-	CreatedAt             time.Time      `db:"CreatedAt"`
-	UpdatedAt             time.Time      `db:"UpdatedAt"`
-	Code                  string         `db:"Code"`
-	PromoCodeID           int64          `db:"PromoCodeId"`
-	FirstName             string         `db:"FirstName"`
-	LastName              string         `db:"LastName"`
-	Email                 string         `db:"Email"`
-	Mobile                string         `db:"Mobile"`
-	AddressLine1          string         `db:"AddressLine1"`
-	AddressLine2          string         `db:"AddressLine2"`
-	City                  string         `db:"City"`
-	State                 string         `db:"State"`
-	Postcode              string         `db:"Postcode"`
-	IP                    string         `db:"IP"`
-	UserAgent             string         `db:"UserAgent"`
-	CountryCode           string         `db:"CountryCode"`
-	PublicKey             string         `db:"PublicKey"`
-	Amount                string         `db:"Amount"`
-	TransferError         sql.NullString `db:"TransferError"`
-	TransferTransactionID sql.NullString `db:"TransferTransactionID"`
+	ID                    int64           `db:"Id"`
+	CreatedAt             time.Time       `db:"CreatedAt"`
+	UpdatedAt             time.Time       `db:"UpdatedAt"`
+	Code                  string          `db:"Code"`
+	PromoCodeID           int64           `db:"PromoCodeId"`
+	FirstName             string          `db:"FirstName"`
+	LastName              string          `db:"LastName"`
+	Email                 string          `db:"Email"`
+	Mobile                string          `db:"Mobile"`
+	AddressLine1          string          `db:"AddressLine1"`
+	AddressLine2          string          `db:"AddressLine2"`
+	City                  string          `db:"City"`
+	State                 string          `db:"State"`
+	Postcode              string          `db:"Postcode"`
+	IP                    string          `db:"IP"`
+	UserAgent             string          `db:"UserAgent"`
+	CountryCode           string          `db:"CountryCode"`
+	PublicKey             string          `db:"PublicKey"`
+	Amount                decimal.Decimal `db:"Amount"`
+	TransferError         sql.NullString  `db:"TransferError"`
+	TransferTransactionID sql.NullString  `db:"TransferTransactionID"`
 }
